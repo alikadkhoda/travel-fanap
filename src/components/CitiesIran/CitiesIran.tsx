9aa589689f3535b055086bf9ee846b5e6e048216
alikadkhoda <kadkhoda.rezaali@gmail.com>
2024-03-29 20:23:55 +0430

    vercel json added

--- src/components/CitiesIran/CitiesIran.tsx
+++ src/components/CitiesIran/CitiesIran.tsx
@@ -25,8 +25,8 @@ export default function CitiesIran() {
             <span className='cities_header_line'></span>
             <div className='morecites'>
                 <Link className='morecities_link' to={'#'}>نمایش بیشتر</Link>
-                <svg className='morecites_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                <svg className='morecites_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                 </svg>
             </div>
             <div className='cities_wrapper'>
